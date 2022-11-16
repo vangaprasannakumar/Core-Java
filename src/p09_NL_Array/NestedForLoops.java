8b3530a064164bcd6a3a877be017c5c52b357de3
Prasanna Kumar Vanga <vangaprasannakumar@gmail.com>
2022-11-16 10:50:37 +0530

    other

--- src/p09_NL_Array/NestedForLoops.java
+++ src/p09_NL_Array/NestedForLoops.java
@@ -1,44 +1,41 @@
 package p09_NL_Array;
 public class NestedForLoops {
 	public static void main(String[] args) {
-		
-		for(int i = 1; i<5; i++) {
-			for(int j = 1; j<5; j++) {
-				System.out.print(j+" ");}
-			System.out.println();}
-		System.out.println();
-		
+		System.out.println("------1-------\n");
 		for(int i = 1; i<=5; i++) {
-			for(int j=1; j<i; j++) {
+			for(int j = 1; j<=5; j++) {
 				System.out.print(j+" ");}
 			System.out.println();}
-		System.out.println();
-		for(int i = 1; i<5; i++) {
+		System.out.println("\n------2-------\n");
+		for(int i = 1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(j+" ");}
+			System.out.println();}
+		System.out.println("\n------3-------\n");
+		for(int i = 1; i<=5; i++) {
 			for(int j = 1; j<=i; j++) {
 				System.out.print(i+" ");}
 			System.out.println();}
-		System.out.println();
-		System.out.println("-------------");
+		System.out.println("\n------4-------\n");
 		int sum = 1;
-		for(int i = 1; i<=5; i++) {
-			for(int j=1; j<i; j++) {
+		for(int i = 1; i<5; i++) {
+			for(int j=1; j<=i; j++) {
 				System.out.print(sum+" ");
 				sum++;}
 			System.out.println();}
-		System.out.println("-------------");
-		System.out.println();
+		System.out.println("\n------5-------\n");
 		
 		for(int i = 1; i<=5; i++) {
 			for(int j = 5; j>=i; j--) {
 				System.out.print(j+" ");}
 			System.out.println();}
-		System.out.println();
+		System.out.println("\n------6-------\n");
 		
 		for(int i = 5; i>=1; i--) {
 			for(int j = 1; j<=i; j++) {
 				System.out.print(j+" ");}
 			System.out.println();}
-		System.out.println();
+		System.out.println("\n------7-------\n");
 		
 		int k = 1;
 		for(int i = 1; i<=5; i++) {
